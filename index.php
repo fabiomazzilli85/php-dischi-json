aff2db82e767be9cf13a2dae2f3ff01b2b04c822
fabiomazzilli85 <155073103+fabiomazzilli85@users.noreply.github.com>
2024-05-08 16:03:17 +0200

    Add files via upload
    Creato index.php, server.php, app.css, app.js. 
    Importato Axios da CDN. Importato Vue JS.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <title>PHP Dischi JSON</title>
+</head>
+<body>
+    <div class="container">
+
+    </div>
+    
+    <script src="app.js"></script>
+</body>
+</html>
